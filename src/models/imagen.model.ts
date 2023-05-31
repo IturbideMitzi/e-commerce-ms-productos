b15a380bea8c9829f12ffd388b421d1f6cfd519b
“Mitzi <mmiturbi@mx1.ibm.com>
2023-05-31 01:02:20 -0600

    datasource, modelos, repositorios y relaciones del microservicio de producto

---
+++ src/models/imagen.model.ts
@@ -0,0 +1,25 @@
+import {Entity, model, property, belongsTo} from '@loopback/repository';
+import {Producto} from './producto.model';
+
+@model()
+export class Imagen extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @belongsTo(() => Producto, {name: 'pertenece_a_producto'})
+  id_producto: number;
+
+  constructor(data?: Partial<Imagen>) {
+    super(data);
+  }
+}
+
+export interface ImagenRelations {
+  // describe navigational properties here
+}
+
+export type ImagenWithRelations = Imagen & ImagenRelations;
